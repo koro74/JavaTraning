bee68096ba8235721a64f0d2c7d145502ad0614f
koro74 <koro74@naver.com>
2025-02-09 04:52:55 +0900

    test

---
+++ ch01/date/1/src/date1/date1.java
@@ -0,0 +1,24 @@
+package date1;
+
+import java.util.Date;
+
+public class date1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date dt = new Date();
+		
+		
+		System.out.println("dt1 >>>>>>>>>>>> " + dt);
+		System.out.println("dt2 >>>>>>>>>>>> " + dt.getDate()); // The method getDate() from the type Date is deprecated 사용안함
+		System.out.println("dt3 >>>>>>>>>>>> " + dt.getDay()); // The method getDate() from the type Date is deprecated 사용안함
+		System.out.println("dt4 >>>>>>>>>>>> " + dt.getHours()); // The method getDate() from the type Date is deprecated 사용안함
+		System.out.println("dt5 >>>>>>>>>>>> " + dt.getMinutes()); // The method getDate() from the type Date is deprecated 사용안함
+		System.out.println("dt6 >>>>>>>>>>>> " + dt.getMonth()); // The method getDate() from the type Date is deprecated 사용안함
+		System.out.println("dt7 >>>>>>>>>>>> " + dt.getSeconds()); // The method getDate() from the type Date is deprecated 사용안함
+		System.out.println("dt8 >>>>>>>>>>>> " + dt.getTime());
+		System.out.println("dt9 >>>>>>>>>>>> " + dt.getTimezoneOffset()); // The method getDate() from the type Date is deprecated 사용안함 
+		System.out.println("dt10 >>>>>>>>>>>> " + dt.getYear()); // The method getDate() from the type Date is deprecated 사용안함
+	}
+
+}
---
+++ ch01/date/2/src/date2/date2.java
@@ -0,0 +1,10 @@
+package date2;
+
+public class date2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello FXXX!");
+	}
+
+}
